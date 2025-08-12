bdbd99437e0a5e7d415c7c1e61f4102de5ff2a5a
Nicolai Friis <nmfr71398@edu.ucl.dk>
2025-08-12 10:34:34 +0200

    Modeller

--- MemorySpil/Models/Cards.cs
+++ MemorySpil/Models/Cards.cs
@@ -1,1 +1,18 @@
 using System;
+
+
+namespace MemorySpilV2.Model
+{
+    public class Cards
+    {
+
+        public int Id { get; set; }
+
+        public string Symbol { get; set; }
+
+        public bool IsFlipped { get; set; }
+
+        public bool IsMatched { get; set; }
+
+    }
+}
---
+++ MemorySpil/Models/GameStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace MemorySpilV2.Model
+{
+    public class Cards
+    {
+        public string PlayerName { get; set; }
+        public int Moves { get; set; }
+        public TimeSpan GameTime { get; set; }
+        public DateTime CompletedAt { get; set; }
+
+    }
+}
